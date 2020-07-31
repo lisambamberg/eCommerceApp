7ab9c7d619409d73284dbcd67cab6b98c946c30d
Lisa Bamberg <lisambamberg@gmail.com>
2020-07-31 14:59:22 -0500

    Setup html form submit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  res.send(`
+<div>
+<form>
+<input name="email" placeholder="email" />
+<input name="password" placeholder="password" />
+<input name="passwordConfirmation" placeholder="password confirmation" />
+<button>Sign Up</button>
+</form>
+</div>
+`);
+});
 
+app.listen(3000, () => {
+  console.log("Listening");
 });
