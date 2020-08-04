4cf411ae50546950e40ca8d299490f5a46e6bcd7
Lisa Bamberg <lisambamberg@gmail.com>
2020-08-04 14:47:40 -0500

    Added methods to find a user by id and delete record

--- repositories/users.js
+++ repositories/users.js
@@ -24,29 +24,39 @@ class UsersRepository {
   }
 
   async create(attributes) {
-    attributes.id = this.randomId();  
+    attributes.id = this.randomId();
     const records = await this.getAll();
     records.push(attributes);
     await this.writeAll(records);
   }
 
   async writeAll(records) {
-    await fs.promises.writeFile(this.filename, JSON.stringify(records, null, 2));
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
   }
 
   randomId() {
-    return crypto.randomBytes(4).toString('hex');
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((record) => record.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id == !id);
+    await this.writeAll(filteredRecords);
   }
 }
 
 const test = async () => {
   const repo = new UsersRepository("users.json");
 
-  await repo.create({ email: "test@test.com", password: "password" });
-
-  const users = await repo.getAll();
-
-  console.log(users);
+  await repo.delete("");
 };
 
 test();
