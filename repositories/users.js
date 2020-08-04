7c6af6232a95ff4a69387696331476a7704d3c78
Lisa Bamberg <lisambamberg@gmail.com>
2020-08-04 11:42:45 -0500

    Setup to save new record to users repository and then return all records

--- repositories/users.js
+++ repositories/users.js
@@ -13,7 +13,31 @@ class UsersRepository {
       fs.writeFileSync(this.filename, "[]");
     }
   }
-  async checkForFile() {}
+
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: "utf-8",
+      })
+    );
+  }
+
+  async create(attributes) {
+    const records = await this.getAll();
+    records.push(attributes);
+
+    await fs.promises.writeFile(this.filename, JSON.stringify(records));
+  }
 }
 
-const repo = new UsersRepository("users.json");
+const test = async () => {
+  const repo = new UsersRepository("users.json");
+
+  await repo.create({ email: "test@test.com", password: "password" });
+
+  const users = await repo.getAll();
+
+  console.log(users);
+};
+
+test();
