6d7726b74f59d8a60216e96d79c92ca5f80635ce
Lisa Bamberg <lisambamberg@gmail.com>
2020-08-07 14:52:29 -0500

    Salting and hashing passwords

--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const crypto = require("crypto");
+const util = require("util");
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -25,11 +28,20 @@ class UsersRepository {
 
   async create(attributes) {
     attributes.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buffer = await scrypt(attributes.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attributes);
+    const record = {
+      ...attributes,
+      password: `${buffer.toString("hex")}.${salt}`,
+    };
+    records.push(record);
+
     await this.writeAll(records);
 
-    return attributes;
+    return record;
   }
 
   async writeAll(records) {
@@ -85,4 +97,4 @@ class UsersRepository {
   }
 }
 
-module.exports = new UsersRepository('users.json');
+module.exports = new UsersRepository("users.json");
