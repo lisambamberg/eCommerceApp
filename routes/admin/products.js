93522abb744da9be6b856310f6c86f4dcecdddf6
Lisa Bamberg <lisambamberg@gmail.com>
2020-08-13 16:43:08 -0500

    Updated middleware

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { validationResult } = require("express-validator");
+const multer = require("multer");
+
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
 const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage });
 
 router.get("/admin/products", (req, res) => {});
 
@@ -12,14 +15,23 @@ router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
 
-router.post("/admin/products/new", [requireTitle, requirePrice], (req, res) => {
-  const errors = validationResult(req);
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  async (req, res) => {
+    const errors = validationResult(req);
 
-  req.on("data", (data) => {
-    console.log(data.toString());
-  });
+    if (!errors.isEmpty()) {
+      return res.send(productsNewTemplate({ errors }));
+    }
 
-  res.send("submitted");
-});
+    const image = console.log(req.file.buffer.toString("base64"));
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -4,9 +4,11 @@ const { getError } = require("../../helpers");
 module.exports = ({ errors }) => {
   return layout({
     content: `
-        <form method="GET"entype="multipart/form-data">
+        <form method="POST"entype="multipart/form-data">
 <input placeholder="Title" name="title" />
+${getError(errors, "title")}
 <input placeholder="Price" name="price" />
+${getError(errors, "price")}
 <input type="file" name="image" />
 <button>Submit</button>
 </form>
